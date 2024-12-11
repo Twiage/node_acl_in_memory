a6d34ad362286ac1e5e4bc6210d1a23b254fe495
Ilya Eremin <IlyaEremin@users.noreply.github.com>
2024-12-11 12:48:27 +0300

    removed redis

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 module.exports = require("./lib/acl.js");
-module.exports.__defineGetter__("redisBackend", function () {
-    return require("./lib/redis-backend.js");
-});
 module.exports.__defineGetter__("memoryBackend", function () {
     return require("./lib/memory-backend.js");
 });
--- test/create-backend.js
+++ test/create-backend.js
@@ -5,18 +5,6 @@ module.exports = async function createBackend(backendType) {
 
     if (backendType === "memory") return new Acl.memoryBackend();
 
-    if (backendType === "redis") {
-        const options = {
-            host: "127.0.0.1",
-            port: 6379,
-            password: null,
-        };
-
-        const redis = await require("redis").createClient(options.port, options.host).connect();
-
-        return new Acl.redisBackend({ redis });
-    }
-
     if (backendType === "mongo") {
         const { MongoClient } = await require("mongodb");
         const client = await MongoClient.connect("mongodb://localhost:27017/acl_test");
@@ -33,5 +21,5 @@ module.exports = async function createBackend(backendType) {
         return new Acl.mongodbBackend({ client, prefix: "acl_", useSingle: true });
     }
 
-    throw new Error("Please assign ACL_BACKEND env const to one of: memory, redis, mongo, mongo_single");
+    throw new Error("Please assign ACL_BACKEND env const to one of: memory, mongo, mongo_single");
 };
